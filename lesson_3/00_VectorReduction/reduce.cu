952bc4df3b25191d72a116f4562856c24a28258b
kroppel <konsti.roppel@t-online.de>
2022-12-19 11:12:10 +0100

    Add solution reduction

--- lesson_3/00_VectorReduction/reduce.cu
+++ lesson_3/00_VectorReduction/reduce.cu
@@ -12,21 +12,62 @@ using namespace timer;
 const int N  = 16777216;
 #define BLOCK_SIZE 256
 
-__global__ void ReduceKernel(int* VectorIN, int N) {
+__global__ void ReduceKernelHighDivergence(int* VectorIN, int N) {
+	__shared__ int SMem[1024];
+	int GlobalIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	SMem[threadIdx.x] = VectorIN[GlobalIndex];
+	__syncthreads();
+	for (int i = 1; i < blockDim.x; i *= 2) {
+		if (threadIdx.x % (i * 2) == 0)
+			SMem[threadIdx.x] += SMem[threadIdx.x + i];
+		__syncthreads();
+	}
+	if (threadIdx.x == 0)
+		VectorIN[blockIdx.x] = SMem[0];
+}
 
+__global__ void ReduceKernelNoDivergence(int* VectorIN, int N) {
+	__shared__ int SMem[1024];
+	int GlobalIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	SMem[threadIdx.x] = VectorIN[GlobalIndex];
+	int accumulator = 0;
+	__syncthreads();
+	for (int i = 1; i < blockDim.x; i *= 2) {
+		accumulator = SMem[threadIdx.x];
+		accumulator += SMem[threadIdx.x + i];
+		__syncthreads();
+		SMem[threadIdx.x] = accumulator;
+		__syncthreads();
+	}
+	if (threadIdx.x == 0)
+		VectorIN[blockIdx.x] = SMem[0];
+}
+
+__global__ void ReduceKernelNoDivergenceV2(int* VectorIN, int N) {
+	__shared__ int SMem[2*1024];
+	int GlobalIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	SMem[threadIdx.x] = VectorIN[GlobalIndex];
+	__syncthreads();
+	for (int i = 1; i < blockDim.x; i *= 2) {
+		//if (threadIdx.x % (i * 2) == 0)
+		SMem[threadIdx.x+((threadIdx.x % (i * 2))+(((threadIdx.x % (i * 2)) != 0)*1024))] += SMem[threadIdx.x + i];
+		__syncthreads();
+	}
+	if (threadIdx.x == 0)
+		VectorIN[blockIdx.x] = SMem[0];
 }
 
 int main() {
     
-    // ------------------- INIT ------------------------------------------------
+    	// ------------------- INIT ------------------------------------------------
 
-    // Random Engine Initialization
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine generator (seed);
-    std::uniform_int_distribution<int> distribution(1, 100);
+    	// Random Engine Initialization
+    	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    	std::default_random_engine generator (seed);
+    	std::uniform_int_distribution<int> distribution(1, 100);
 
-    Timer<HOST> host_TM;
-    Timer<DEVICE> dev_TM;
+    	Timer<HOST> host_TM;
+    	Timer<DEVICE> dev_TM;
 
 	// ------------------ HOST INIT --------------------------------------------
 
@@ -45,59 +86,69 @@ int main() {
 	int sum;
 	float dev_time;
 
-	// ------------------- CUDA COMPUTATION 1 ----------------------------------
+	// ------------------- CUDA COMPUTATION  ----------------------------------
 
-    std::cout<<"Starting computation on DEVICE "<<std::endl;
+    	std::cout<<"Starting computation on DEVICE "<<std::endl;
 
-    dev_TM.start();
-	ReduceKernel<<<DIV(N, BLOCK_SIZE), BLOCK_SIZE>>>
-                        (devVectorIN, N);
-	ReduceKernel<<<DIV(N, BLOCK_SIZE* BLOCK_SIZE), BLOCK_SIZE>>>
-                         (devVectorIN, DIV(N, BLOCK_SIZE));
-	ReduceKernel<<<DIV(N, BLOCK_SIZE * BLOCK_SIZE * BLOCK_SIZE), BLOCK_SIZE>>>
-                         (devVectorIN, DIV(N, BLOCK_SIZE * BLOCK_SIZE));
+    	dev_TM.start();
+	/*
+	ReduceKernelHighDivergence<<<DIV(N, BLOCK_SIZE), BLOCK_SIZE>>>
+		(devVectorIN, N);
+
+	ReduceKernelHighDivergence<<<DIV(N, BLOCK_SIZE* BLOCK_SIZE), BLOCK_SIZE>>>
+	 	(devVectorIN, DIV(N, BLOCK_SIZE));
+	ReduceKernelHighDivergence<<<DIV(N, BLOCK_SIZE * BLOCK_SIZE * BLOCK_SIZE), BLOCK_SIZE>>>
+	 	(devVectorIN, DIV(N, BLOCK_SIZE * BLOCK_SIZE));
+	*/
+	ReduceKernelNoDivergenceV2<<<DIV(N, BLOCK_SIZE), BLOCK_SIZE>>>
+		(devVectorIN, N);
+
+	ReduceKernelNoDivergenceV2<<<DIV(N, BLOCK_SIZE* BLOCK_SIZE), BLOCK_SIZE>>>
+	 	(devVectorIN, DIV(N, BLOCK_SIZE));
+	ReduceKernelNoDivergenceV2<<<DIV(N, BLOCK_SIZE * BLOCK_SIZE * BLOCK_SIZE), BLOCK_SIZE>>>
+	 	(devVectorIN, DIV(N, BLOCK_SIZE * BLOCK_SIZE));
 
 	dev_TM.stop();
 	dev_time = dev_TM.duration();
 	CHECK_CUDA_ERROR;
 
 	SAFE_CALL( cudaMemcpy(&sum, devVectorIN, sizeof(int),
-                            cudaMemcpyDeviceToHost) );
+	    	cudaMemcpyDeviceToHost) );
 
 	// ------------------- HOST ------------------------------------------------
-    host_TM.start();
+	host_TM.start();
 
 	int host_sum = std::accumulate(VectorIN, VectorIN + N, 0);
 
-    host_TM.stop();
+	host_TM.stop();
 
-    std::cout << std::setprecision(3)
-              << "KernelTime Divergent: " << dev_time << std::endl
-              << "HostTime            : " << host_TM.duration() << std::endl
-              << std::endl;
+	std::cout << std::setprecision(3)
+	      << "KernelTime Divergent: " << dev_time << std::endl
+	      << "HostTime            : " << host_TM.duration() << std::endl
+	      << std::endl;
 
-    // ------------------------ VERIFY -----------------------------------------
+	// ------------------------ VERIFY -----------------------------------------
 
-    if (host_sum != sum) {
-        std::cerr << std::endl
-                  << "Error! Wrong result. Host value: " << host_sum
-                  << " , Device value: " << sum
-                  << std::endl << std::endl;
-        cudaDeviceReset();
-        std::exit(EXIT_FAILURE);
-    }
+	if (host_sum != sum) {
+	std::cerr << std::endl
+		  << "Error! Wrong result. Host value: " << host_sum
+		  << " , Device value: " << sum
+		  << std::endl << std::endl;
+	cudaDeviceReset();
+	std::exit(EXIT_FAILURE);
+	}
 
-    //-------------------------- SPEEDUP ---------------------------------------
+	//-------------------------- SPEEDUP ---------------------------------------
 
-    float speedup = host_TM.duration() / dev_time;
+	float speedup = host_TM.duration() / dev_time;
 
-    std::cout << "Correct result" << std::endl
-              << "Speedup achieved: " << std::setprecision(3)
-              << speedup << " x" << std::endl << std::endl;
+	std::cout << "Correct result" << std::endl
+	      << "Speedup achieved: " << std::setprecision(3)
+	      << speedup << " x" << std::endl << std::endl;
 
-    std::cout << host_TM.duration() << ";" << dev_TM.duration() << ";" << host_TM.duration() / dev_TM.duration() << std::endl;
+	std::cout << host_TM.duration() << ";" << dev_TM.duration() << ";" << host_TM.duration() / 			dev_TM.duration() << std::endl;
 
-    delete[] VectorIN;
-    SAFE_CALL( cudaFree(devVectorIN) );
-    cudaDeviceReset();
-}
+	delete[] VectorIN;
+	SAFE_CALL( cudaFree(devVectorIN) );
+	cudaDeviceReset();
+	}
